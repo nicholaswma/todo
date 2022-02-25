67a64203479146bcaec6eb7aa471355c4ff83222
Nicholas Ma <nicholasma@nicholass-mbp.lan>
2022-02-24 20:36:01 -0800

    updated frontend

--- todo-fe/src/App.tsx
+++ todo-fe/src/App.tsx
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Flex, Text, Grid, GridItem, Box, Center } from "@chakra-ui/react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Grid templateColumns="repeat(5, 1fr)" paddingTop={"10rem"}>
+        <GridItem colSpan={1} background={"white"}></GridItem>
+        <GridItem colSpan={4}>
+          <Box h="85vh" bg="#F1F1F1" borderRadius={"25px"}>
+            <Flex
+              flexDir={"row"}
+              justifyContent={"space-between"}
+              padding={"4rem"}
+            >
+              <Box>
+                <Text fontWeight={800} fontFamily={"Poppins"} fontSize={"36px"}>
+                  Today
+                </Text>
+                <Text fontFamily={"Poppins"} fontSize={"20px"}>
+                  Wed Feb 24 2022
+                </Text>
+              </Box>
+              <Flex borderRadius={"25px"} bg="white" w="450px">
+                <Center
+                  fontWeight={800}
+                  fontFamily={"Poppins"}
+                  fontSize={"20px"}
+                  paddingLeft={"30px"}
+                >
+                  Start Time Tracker
+                  <Box bg="#FABB18" w="50px" h="50px"></Box>
+                </Center>
+              </Flex>
+            </Flex>
+          </Box>
+        </GridItem>
+      </Grid>
     </div>
   );
 }
---
+++ todo-fe/src/Tasks.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Flex, Text, Box, Grid, GridItem } from "@chakra-ui/react";
+
+function Tasks() {
+  return <Flex height={"100vh"} background={"gray.300"}></Flex>;
+}
+
+export default Tasks;
